432a32cf94be326c469a0e034e35cd3ffb9fde97
JosephPR <reilly4181@gmail.com>
2019-10-31 02:22:54 -0600

    jwt web token, bcrypt, passport setup, added register and login routes

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,14 +3,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
+  name: {
     type: String,
     required: true,
-    unique: true,
-    trim: true,
-    minlength: 3
+    unique: true
+
   },
-}, {
+  email: {
+   type: String,
+   required: true,
+   unique: true
+ },
+ password: {
+   type: String,
+   required: true
+ },
+ date: {
+    type: Date,
+    default: Date.now
+  }
+},
+ {
   timestamps: true,
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/users");
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
@@ -23,6 +32,11 @@ connection.once('open', () => {
 const jobsRouter = require('./routes/jobs');
 const usersRouter = require('./routes/users');
 
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
 app.use('/jobs', jobsRouter);
 app.use('/users', usersRouter);
 
